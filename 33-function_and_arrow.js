2aab59e477309d3a8756dceee2a902ac4354f241
qiaochao <752716886@qq.com>
2021-11-26 11:51:01 +0800

    arrow

--- 33-function_and_arrow.js
+++ 33-function_and_arrow.js
@@ -19,3 +19,16 @@ const person1 = {
 //undefined,why? 虽然箭头函数没有this，但它不是指向了person1了吗？
 //再学习一下箭头函数
 person1.print();
+
+
+//还有一种情况
+var obj = {
+    birth: 1990,
+    getAge: function (year) {
+        var b = this.birth; // 1990
+        var fn = (y) => y - this.birth; // this.birth仍是1990
+        return fn.call({birth:2000}, year);
+    }
+};
+const result =  obj.getAge(2015); // 25
+console.log("---",result);
